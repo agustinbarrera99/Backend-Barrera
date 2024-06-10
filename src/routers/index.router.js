2a69ecfffbd788d6820484bf188e2c92a1e62c66
agustinbarrera99 <agustinbarrera.ri@gmail.com>
2024-06-10 14:35:10 -0300

    paginacion y filtrado en fs

--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -1,6 +1,5 @@
 import CustomRouter from "./CustomRouter.js";
 import ApiRouter from "./api/index.router.js"
-import ViewsRouter from "./views/index.view.js";
 
 const api = new ApiRouter()
 const views = new ViewsRouter()
@@ -8,7 +7,6 @@ const views = new ViewsRouter()
 class IndexRouter extends CustomRouter {
   init() {
     this.router.use("/api", api.getRouter())
-    this.router.use("/", views.getRouter())
   }
 }
 
